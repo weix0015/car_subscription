897843fc87329e3c114e04ebe094a43e99235d38
weix0015 <weix0015@stud.kea.dk>
2023-05-05 11:00:20 +0200

    car_subscription connect to GitHub

---
+++ src/main/java/com/example/car_subscription/CarSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSubscriptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/car_subscription/CarSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSubscriptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

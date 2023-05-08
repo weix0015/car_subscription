97697d5270999f4a079ef414ec0d89a4da27a3f5
weix0015 <weix0015@stud.kea.dk>
2023-05-08 11:04:07 +0200

    Merge feature_wei branch into main branch

--- src/main/java/com/example/car_subscription/Model/Customer.java
+++ src/main/java/com/example/car_subscription/Model/Customer.java
@@ -1,5 +1,78 @@
 package com.example.car_subscription.Model;
 
 public class Customer {
+    private int customer_id;
+    private String first_name;
+    private String last_name;
+    private String address;
+    private int zipcode;
+    private String city;
+    private String mail;
 
+    public Customer(int customer_id, String first_name, String last_name,
+                    String address, int zipcode, String city, String mail) {
+        this.customer_id = customer_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.city = city;
+        this.mail = mail;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
 }

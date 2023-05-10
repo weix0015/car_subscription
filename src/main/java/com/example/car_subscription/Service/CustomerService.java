978a864761ca76bd8caba7ad74b3770b7972345d
lillebitteditte <ditt2047@stud.kea.dk>
2023-05-10 11:08:59 +0200

    CustomerCRUD

--- src/main/java/com/example/car_subscription/Service/CustomerService.java
+++ src/main/java/com/example/car_subscription/Service/CustomerService.java
@@ -1,4 +1,30 @@
 package com.example.car_subscription.Service;
 
+import com.example.car_subscription.Model.Customer;
+import com.example.car_subscription.Repository.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class CustomerService {
+    
+    @Autowired
+    CustomerRepo customerRepo;
+
+    public List<Customer> fetchAll() {
+        return customerRepo.fetchAll();
+    }
+
+    public void addCustomer(Customer c){
+        customerRepo.addCustomer(c);
+    }
+
+    public void editCustomer(int id, Customer c){
+        customerRepo.editCustomer(id, c);
+    }
+
+    public Boolean deleteCustomer(int id){
+        return customerRepo.deleteCustomer(id);
+    }
+
 }

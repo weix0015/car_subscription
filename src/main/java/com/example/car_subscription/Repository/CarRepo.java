742263461af5060e9707d4520753241dfe04934f
weix0015 <weix0015@stud.kea.dk>
2023-05-26 11:00:15 +0200

    added notRentedTotalCar

--- src/main/java/com/example/car_subscription/Repository/CarRepo.java
+++ src/main/java/com/example/car_subscription/Repository/CarRepo.java
@@ -66,4 +66,11 @@ public class CarRepo {
         RowMapper<Car> rowMapper = new BeanPropertyRowMapper<>(Car.class);
         return template.query(sql, rowMapper);
     }
+
+    // total car not rented in car table
+    public List<Car> notRentedTotalCar(int car_id, Car car) {
+        String sql = "SELECT * COUNT(car_id) AS notRentedTotalCar WHERE rented = 1;";
+        RowMapper<Car> rowMapper = new BeanPropertyRowMapper<>(Car.class);
+        return template.query(sql, rowMapper);
+    }
 }
